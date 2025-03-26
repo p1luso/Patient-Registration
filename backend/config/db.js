83a5c237594f44083f361208507a30fe9b34fbc1
Santiago Perez Delfino <santiago@utriper.com>
2025-03-25 23:04:50 -0300

    fix: update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres', // Cambia esto por tu usuario de PostgreSQL
-    host: 'db',
-    database: 'postgres', // El nombre de la base de datos que creaste
-    password: 'nilda1668', // Cambia esto por tu contraseña de PostgreSQL
-    port: 5432,
+    user: 'patient_user', 
+    host: 'db',          
+    database: 'patient_db', 
+    password: 'patient_password', 
+    port: 5432,           
 });
 
 module.exports = pool;
